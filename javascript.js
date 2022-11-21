d1980b06c66f983709ce1311f8943196818e2bb4
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-11-20 19:27:58 -0500

    If applied, will apply css styling

--- javascript.js
+++ javascript.js
@@ -1,38 +1,39 @@
 let myLibrary = ["hello", "wassup"];
 
-function Book(...book) { //the constructor
-    this.book = book;
+function Book(title, author, pages, read) { //the constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
-let addStuff = new Book(myLibrary);
-console.log(addStuff);
 
-
-function addBookToLibrary() {
-    let type = document.querySelector("tr");
-
-    let create = document.createElement("th");
-    create.classList.add("books");
-    
-    let cells = create.textContent = "hi";
-
-    type.appendChild(create);
-
-    for (let i = 0; i < myLibrary.length; i++) {
-        cells[i];
-        console.log(myLibrary);
-    }
-}
-
-addBookToLibrary();
-
-let press = document.querySelector(".press");
+let press = document.querySelector(".press"); //will create button
 
 let enter = document.createElement("button");
 enter.classList.add("submit");
-//enter.addEventListener("click");
+enter.textContent = "Add Books";
+enter.addEventListener("click", inputText);
 
 press.appendChild(enter);
+
+
+
+function inputText() {
+    let bookInfo = window.prompt("input the title, author, pages, and read/not read");
+    myLibrary.push(bookInfo);
+    console.log(myLibrary);
+
+    let type = document.querySelector(".add");
+
+    let create = document.createElement("td");
+    create.classList.add("books");
+    
+    let cells = create.textContent = bookInfo;
+    let table = new Book(bookInfo);
+
+    type.appendChild(create);
+}
 
 
 // function theHobbit(title, author, pages, read) {
