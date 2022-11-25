32e18aab081cb37d370fe942574297a6c1a6d659
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-11-25 12:48:32 -0500

    If applied will add a toggle button to the read cells

--- javascript.js
+++ javascript.js
@@ -1,45 +1,85 @@
-let myLibrary = ["hello", "wassup"];
+let myLibrary = [];
 
 function Book(title, author, pages, read) { //the constructor
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
 
+    let type = document.querySelector(".cell1"); //will add cell to title 
+    let create = document.createElement("td"); //creates the cell elements
+    create.classList.add("books");
+    create.textContent = title;
+    type.appendChild(create);
+
+    let authorEl = document.querySelector(".cell2");
+    let create2 = document.createElement("td");
+    create2.classList.add("books");
+    create2.textContent = author;
+    authorEl.appendChild(create2);
+
+    let pageEl = document.querySelector(".cell3");
+    let create3 = document.createElement("td");
+    create3.classList.add("books");
+    create3.textContent = pages;
+    pageEl.appendChild(create3);
+
+    let readEl = document.querySelector(".cell4");
+    let create4 = document.createElement("td");
+    create4.classList.add("books");
+    create4.textContent = read;
+    readEl.appendChild(create4);
+
+    let remove = document.querySelector(".changeRead");
+    let button = document.createElement("button");
+    button.classList.add("remove");
+    button.textContent = "toggle read";
+    remove.appendChild(button);
+}
 
 let press = document.querySelector(".press"); //will create new book button
 
 let enter = document.createElement("button");
 enter.classList.add("submit");
 enter.textContent = "New Book";
-enter.addEventListener("click", inputText); //triggers function when new book button is clicked
+enter.addEventListener("click", openForm); //triggers function when new book button is clicked
 
 press.appendChild(enter);
 
 
+function displayBook() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+    }
+}
 
-function inputText() {
-    document.getElementById("myForm").style.display = "block"; //will open form
+function openForm() {
+    document.getElementById("myForm").style.display = "block"; 
 
-    //let bookInfo = window.prompt("input the title, author, pages, and read/not read");
+    let complete = document.querySelector(".btn"); //will access the submit button on form
+    complete.addEventListener("click", addBookToLibrary);
+}
+
+
+function addBookToLibrary() {
+    let title = document.querySelector(".title").value;
+    let author = document.querySelector(".author").value;
+    let pages = document.querySelector(".pages").value;
+    let read = document.querySelector(".read").value;
+    console.log(title);
+
+    let bookInfo = new Book(title, author, pages, read); //will add new book to array
     myLibrary.push(bookInfo);
     console.log(myLibrary);
 
-    let type = document.querySelector(".title"); //will add cell to title 
+    displayBook();
 
-    let create = document.createElement("td");
-    create.classList.add("books");
-    
-    let cells = create.textContent = bookInfo;
-    let table = new Book(bookInfo);
-
-    type.appendChild(create);
+    event.preventDefault(); //will prevent form submission page refresh
 }
   
-  function closeForm() {
+function closeForm() {
     document.getElementById("myForm").style.display = "none";
-  }
+}
 
 
 // function theHobbit(title, author, pages, read) {
